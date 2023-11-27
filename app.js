58e02d806fa4a2dfdb4dd7aaa226b1dc7a1ada01
Nx0dle <ramenkeebs@proton.me>
2023-11-27 18:18:25 +0100

    blur hero bg while scrolling

---
+++ app.js
@@ -0,0 +1,7 @@
+let heroCover = document.getElementById('hero-cover')
+let blur = 1
+
+document.addEventListener("scroll", () => {
+    let value = window.scrollY
+    heroCover.style.backdropFilter = `blur(${value * 0.03}px)`
+});
